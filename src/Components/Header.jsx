d2eaf8868b745250bb33464928f0957b4a9d4973
Karan Vaswani <karanvaswani2002@gmail.com>
2024-01-09 03:49:56 +0530

    removed warnings

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Input, InputGroup, InputLeftElement, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Header = () => {
