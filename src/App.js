1ec821b624b55d72b5a165ffd05949c548dbb1c9
Karan Vaswani <karanvaswani2002@gmail.com>
2024-01-09 03:42:02 +0530

    completed ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import MainRoutes from './Routes/MainRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", height: "100vh", margin: "0px", padding: "0px", width: "100%" }}>
+      {/* <style>
+        {`
+         
+         @import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@900&display=swap');
+      
+          body {
+            font-family: 'Darker Grotesque', sans-serif;
+          }
+        `}
+      </style> */}
+      <div style={{ width: "224px", backgroundColor: "#1E2640", height: "100vh", justifyContent: "space-between" }}>
+        <Navbar />
+      </div>
+      <div style={{ width: "calc(100% - 224px)", backgroundColor: "#FAFAFA" }}>
+        <MainRoutes />
+      </div>
     </div>
   );
 }
